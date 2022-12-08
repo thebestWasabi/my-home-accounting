8b08840250004c50b7b91735674fb778b358f9c7
thebestWasabi <openpw@icloud.com>
2022-12-08 17:41:27 +0300

    0.0.2

--- src/main/java/model/Article.java
+++ src/main/java/model/Article.java
@@ -2,6 +2,8 @@ package model;
 
 import exception.ModelException;
 
+import java.util.Objects;
+
 public class Article extends Common {
 
     private String title;
@@ -10,8 +12,40 @@ public class Article extends Common {
     }
 
     public Article(String title) throws ModelException {
-        if (title.length() == 0)
-            throw new ModelException(ModelException.TITLE_EMPTY);
+        if (title.length() == 0) throw new ModelException(ModelException.TITLE_EMPTY);
+        this.title = title;
+    }
+
+    @Override
+    public String getValueForComboBox() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(title, article.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
     }
 }
